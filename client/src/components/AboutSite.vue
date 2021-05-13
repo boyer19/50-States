9e7b2f2b0c6d51ef794cbec60bf311739b2f0c96
Matt <ok7140mj@go.minnstate.edu>
2021-05-13 13:47:44 -0500

    comment on AboutSite.vue

--- client/src/components/AboutSite.vue
+++ client/src/components/AboutSite.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h2>Where have you traveled to?</h2>
-
+<!-- Would like to add picture here -->
         <p>Use this site to keep track of which States you have traveled to.</p>
         <p>Click the checkbox to mark which States you have visited.</p>
         <p>Click on the map icon and see where that State is located.</p>
